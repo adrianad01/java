99f1d12445166d72c7582e90f60464fec19ff578
Adrian Alcocer <jadrianaldo@outlook.com>
2024-02-22 18:58:31 -0600

    commit

--- HolaMundo/src/Sentencias/SentenciaFor.java
+++ HolaMundo/src/Sentencias/SentenciaFor.java
@@ -2,11 +2,11 @@ package Sentencias;
 
 public class SentenciaFor {
     public static void main(String[] args) {
-        for (int i = 0; i <= 5; i++){
+        for (int i = 0; i <= 10; i++){
             System.out.println("i = " + i);
         }
-        for (int i = 10; i >= 0; i--){
-            System.out.println("i = " + i);
+        for (int j = 10; j >= 0; j--){
+            System.out.println("j = " + j);
         }
     }
 }
---
+++ HolaMundo/src/W3SchoolsExcersices/Ejercicio1.java
@@ -0,0 +1,7 @@
+package W3SchoolsExcersices;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
