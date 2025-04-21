c46772a3492fe871b6c2b8d02c656eadd483d1d7
Adrian Alcocer <jalcocer@bancobase.com>
2025-04-21 16:12:33 -0600

    lambda

---
+++ src/lambda/LambdaExamples.java
@@ -0,0 +1,18 @@
+package lambda;
+
+import java.util.Comparator;
+
+public class LambdaExamples {
+
+    public static void main(String[] args) {
+
+        Comparator<String> stringComparator = new Comparator<String>() {
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        };
+
+        int comparison = stringComparator.compare("hello", "world");
+        System.out.println(comparison);
+    }
+}
