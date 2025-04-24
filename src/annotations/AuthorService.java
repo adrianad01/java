4775bf554a2c0ba1ea80f979bf997231ee9936bb
Adrian Alcocer <jalcocer@bancobase.com>
2025-04-24 10:55:28 -0600

    add annotations package

---
+++ src/annotations/AuthorService.java
@@ -0,0 +1,19 @@
+package annotations;
+
+import java.lang.reflect.Method;
+
+public class AuthorService {
+
+    @Author(author = "Adrian", date = "2021-01-01")
+    public void readAuthor(){
+        System.out.println("Reading author...");
+    }
+
+    public static void main(String[] args) throws Exception{
+        Method method = AuthorService.class.getMethod("readAuthor");
+        Author annotation = method.getAnnotation(Author.class);
+        assert annotation != null;
+        System.out.println(annotation.author());
+        System.out.println(annotation.date());
+    }
+}
